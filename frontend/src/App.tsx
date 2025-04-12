5b9942bb159d685bf51a2d049333a0fc24abf9f2
ankur-2213 <ankurarora1002@gmail.com>
2025-04-12 16:28:15 +0530

    final bugs fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
+        <Routes >
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
